dc7f2652761f33376bfe9da33bf42de07d7ffa5d
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-05-01 00:02:11 +0300

    axios setup

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,4 +7,16 @@ const api = axios.create({
     },
 });
 
+// Requesting interceptor to attach JWT Token
+api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+);
+
 export default api;
