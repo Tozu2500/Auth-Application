e97a46a8f504464cd54ccd65429f834a8795f47e
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-05-08 02:14:07 +0300

    Bug and typo fises

--- backend/src/main/java/com/auth/app/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/auth/app/repository/RefreshTokenRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.auth.app.entity.RefreshToken;
@@ -17,5 +18,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByUser(User user);
 
     @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user = :user")
     void deleteByUser(User user);
 }
